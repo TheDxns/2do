849bb0d1eea23dad18b927019bd60fdad90257cf
TheDxns <denis.lukasczyk@gmail.com>
2023-04-23 19:58:38 +0200

    removing proxy

--- src/frontend/src/Dashboard.vue
+++ src/frontend/src/Dashboard.vue
@@ -183,14 +183,14 @@ export default {
     },
     methods: {
       getLists() {
-        fetch("/api/lists/permitted/" + this.$keycloak.idTokenParsed.sub)
+        fetch("http://localhost:9000/api/lists/permitted/" + this.$keycloak.idTokenParsed.sub)
           .then((response) => response.json())
           .then((data) => {
             this.lists = data;
           })
       },
       createNewCustomList() {
-          fetch("/api/lists", {
+          fetch("http://localhost:9000/api/lists", {
                   method: 'post',
                   headers: {
                     'Accept': 'application/json',
